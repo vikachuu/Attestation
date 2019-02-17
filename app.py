6829698978f7a602dd8ae285f4c922ec9695319e
vikachuu <kozopasv@gmail.com>
2019-02-18 00:39:19 +0200

    test cors

--- app.py
+++ app.py
@@ -46,6 +46,21 @@ def ping():
     return jsonify({"status": 200, "msg":"This message is coming from Flask backend!"})
 
 
+@app.after_request
+def add_cors(resp):
+    """ Ensure all responses have the CORS headers. This ensures any failures are also accessible
+        by the client. """
+    resp.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin','*')
+    resp.headers['Access-Control-Allow-Credentials'] = 'true'
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+    resp.headers['Access-Control-Allow-Headers'] = request.headers.get(
+        'Access-Control-Request-Headers', 'Authorization' )
+    # set low for debugging
+    if app.debug:
+        resp.headers['Access-Control-Max-Age'] = '1'
+    return resp
+
+
 if __name__ == ' __main__':
     #app.debug = True
     app.run()
