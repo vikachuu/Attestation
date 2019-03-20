392160ffef2b1362c3079668a7be401639411940
vikachuu <kozopasv@gmail.com>
2019-03-20 08:45:09 +0200

    left one model in class to test

--- web/model/application_models.py
+++ web/model/application_models.py
@@ -29,26 +29,24 @@ class ExtraApplication(db.Model):
         self.personnel_number = personnel_number
 
 
-class DefermentApplication(db.Model):
-    __tablename__ = "deferment_application"
-
-    deferment_application_number = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    deferment_application_date = db.Column(db.Date, nullable=False)
-    deferment_application_reason = db.Column(db.String(1500), nullable=False)
-    deferment_application_years = db.Column(db.Integer, nullable=False)
-    deferment_application_status = db.Column(db.String(50), nullable=False)
-
-    personnel_number = db.Column(db.Integer, db.ForeignKey('teacher.personnel_number', onupdate="CASCADE",
-                                                           ondelete="CASCADE"), nullable=False)
-    teacher = db.relationship("Teacher", back_populates="deferment_application")
-
-    def __init__(self, deferment_application_date, deferment_application_reason, deferment_application_years,
-                 deferment_application_status, personnel_number):
-        self.deferment_application_date = datetime.strptime(deferment_application_date, '%Y-%m-%d').date()
-        self.deferment_application_reason = deferment_application_reason
-        self.deferment_application_years = deferment_application_years
-        self.deferment_application_status = STATUS[deferment_application_status].value
-
-        self.personnel_number = personnel_number
-
-
+# class DefermentApplication(db.Model):
+#     __tablename__ = "deferment_application"
+#
+#     deferment_application_number = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     deferment_application_date = db.Column(db.Date, nullable=False)
+#     deferment_application_reason = db.Column(db.String(1500), nullable=False)
+#     deferment_application_years = db.Column(db.Integer, nullable=False)
+#     deferment_application_status = db.Column(db.String(50), nullable=False)
+#
+#     personnel_number = db.Column(db.Integer, db.ForeignKey('teacher.personnel_number', onupdate="CASCADE",
+#                                                            ondelete="CASCADE"), nullable=False)
+#     teacher = db.relationship("Teacher", back_populates="deferment_application")
+#
+#     def __init__(self, deferment_application_date, deferment_application_reason, deferment_application_years,
+#                  deferment_application_status, personnel_number):
+#         self.deferment_application_date = datetime.strptime(deferment_application_date, '%Y-%m-%d').date()
+#         self.deferment_application_reason = deferment_application_reason
+#         self.deferment_application_years = deferment_application_years
+#         self.deferment_application_status = STATUS[deferment_application_status].value
+#
+#         self.personnel_number = personnel_number
